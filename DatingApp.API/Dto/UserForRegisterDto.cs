6776ac8c3f78abf2f1b12ece219b7cb37727d74b
Kevin Do <hoangtuantx92@yahoo.com>
2019-07-13 20:51:32 -0500

    update with auth

--- DatingApp.API/Dto/UserForRegisterDto.cs
+++ DatingApp.API/Dto/UserForRegisterDto.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dto
 {
     public class UserForRegisterDto
     {
+        [Required]
+        public string Username { get; set; }    
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "The password need to be between 4 to 8 characters")]
+        public string Password { get; set; }    
         
     }
 }
