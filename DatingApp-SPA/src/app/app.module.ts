5f582c7e154d24d07cb34dde52c035dcfaf33faa
Kevin Do <hoangtuantx92@yahoo.com>
2019-07-25 19:08:26 -0500

    add function for user to edit profile

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListlResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './member/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -68,7 +69,8 @@ export function tokenGetter() {
       UserService,
       MemberDetailResolver,
       MemberListlResolver,
-      MemberEditResolver
+      MemberEditResolver,
+      PreventUnsavedChanges
    ],
    bootstrap: [
       AppComponent
