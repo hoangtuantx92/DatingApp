cd02720fe5d6d00282c25e2e8346421125047d55
Kevin Do <hoangtuantx92@yahoo.com>
2019-07-26 19:42:05 -0500

    added  function in photos controller

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -35,6 +35,7 @@ namespace DatingApp.API.Controllers {
 
             _cloudinary = new Cloudinary(acc);
         }
+
         [HttpGet("{id}", Name = "GetPhoto")]
         public async Task<IActionResult> GetPhoto(int id)
         {
@@ -85,7 +86,8 @@ namespace DatingApp.API.Controllers {
 
             if (await _repo.SaveAll())
             {
-                return CreatedAtRoute()
+                var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
+                return CreatedAtRoute("GetPhoto", new { id = photo.id}, photoToReturn);
             }
 
             return BadRequest("Could not add the photo");
