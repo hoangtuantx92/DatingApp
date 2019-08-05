d148be16bc80bc28e57b568f49fbef6f77fdd623
Kevin Do <hoangtuantx92@yahoo.com>
2019-08-05 13:30:43 -0500

    fix error for pagination

--- DatingApp-SPA/src/app/member/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/member/member-list/member-list.component.ts
@@ -3,6 +3,7 @@ import { User } from '../../_models/user';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination } from 'src/app/_models/Pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -11,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  pagination: Pagination;
 
   constructor(private userService: UserService, private alertify: AlertifyService,
     private route: ActivatedRoute) { }
@@ -18,7 +20,13 @@ export class MemberListComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    console.log(this.pagination.currentPage);
   }
 
   // loadUsers() {
