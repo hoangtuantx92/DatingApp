80ac164d3e15487549ad30516107f6a69d3d7b34
Kevin Do <hoangtuantx92@yahoo.com>
2019-08-07 17:22:57 -0500

    add like filter to spa

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -58,4 +58,9 @@ setMainPhoto(userId: number, id: number) {
 deletePhoto(userId: number, id: number) {
   return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
 }
+
+sendLike(id: number, recipientId: number) {
+  return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
+}
+
 }
