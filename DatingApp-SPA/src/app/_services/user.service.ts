f9d0aac69500799d3ae0e92548d8993d9c0e8517
Kevin Do <hoangtuantx92@yahoo.com>
2019-08-19 14:31:49 -0500

    add get message thread to user service

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -96,4 +96,8 @@ getMessages(id: number, page?, itemsPerPage?, messageContainer?) {
       })
     );
 }
+
+getMessageThread(id: number, recipientId: number) {
+  return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId);
+}
 }
