b3596ed5e8479b59692eb576bda2876594aafee5
Kevin Do <hoangtuantx92@yahoo.com>
2019-08-28 14:13:32 -0500

    added send message function

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -100,4 +100,8 @@ getMessages(id: number, page?, itemsPerPage?, messageContainer?) {
 getMessageThread(id: number, recipientId: number) {
   return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId);
 }
+
+sendMessage(id: number, message: Message) {
+  return this.http.post(this.baseUrl + 'users/' + id + '/messages', message);
+}
 }
