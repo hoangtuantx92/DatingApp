b65a1e38e8acadac2d70e00fa2f051ee0467a175
Kevin Do <hoangtuantx92@yahoo.com>
2019-08-29 16:31:11 -0500

    update get message thread

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -156,8 +156,10 @@ namespace DatingApp.API.Data
             var messages = await _context.Messages
                 .Include(u => u.Sender).ThenInclude(p => p.Photos)
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-                .Where(m => m.RecipientId == userId && m.SenderId == recipientId 
-                    || m.RecipientId == recipientId && m.SenderId == userId)
+                .Where(m => m.RecipientId == userId && m.RecipientDeleted == false
+                     && m.SenderId == recipientId 
+                    || m.RecipientId == recipientId && m.SenderId == userId
+                     && m.SenderDeleted == false)
                 .OrderByDescending(m => m.MessageSent)
                 .ToListAsync();
 
